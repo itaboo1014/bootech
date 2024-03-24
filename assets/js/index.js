498e57102e0fc833016916ea5fd24e6f01527c0a
itaboo <itaboo@itaboononotobukkukonpyuta.local>
2024-03-24 17:04:41 +0900

    test

---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+const { createClient } = microcms;
+
+// Initialize Client SDK.
+const client = createClient({
+    serviceDomain: "bootech",
+    apiKey: "GaFOid11lJlYcjzqaZ3oOLmpSbzxm1q6lDjp"
+});
+
+client
+    .get({
+        endpoint: 'news',
+        // queries: {
+        //     limit: 100,
+        //     offset: 2,
+        //     orders: '-publishedAt',
+        //     q: 'キーワード',
+        //     filters:
+        //     fields:
+        //     ids:
+        // }
+    })
+    .then((news) => {
+        console.log(news);
+    })
+    .catch(error => console.error('Error:', error))
+;
+
+client
+    .get({
+        endpoint: 'blogs',
+        queries: {
+            limit: 100
+        }
+    })
+    .then((blogs) => {
+        console.log(blogs);
+    })
+    .catch(error => console.error('Error:', error))
+;
+
+client
+    .get({
+        endpoint: 'categories',
+        queries: {
+            limit: 100
+        }
+    })
+    .then((categories) => {
+        console.log(categories);
+    })
+    .catch(error => console.error('Error:', error))
+;
